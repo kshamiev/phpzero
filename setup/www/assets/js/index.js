cbca0439a8e1bf177bdab4b8452d5f7c67830d7d
Konstantin Shamiev <kshamiev@hostkey.ru>
2015-08-21 18:17:10 +0300

    feature

--- setup/www/assets/js/index.js
+++ setup/www/assets/js/index.js
@@ -40,3 +40,28 @@ jQuery(".timepicker").timepicker({
     language: 'ru'
     });
 });
+//	новое окно
+function system_window(path, target, w, h, pos, face)
+{
+    if ( pos==1 )
+    { posx=screen.width/2-w/2; posy=screen.height/2-h/2; }
+    else
+    { posx=0; posy=0; }
+    if ( face==1 )
+    { newWindow=window.open(path,target,"left="+posx+", top="+posy+", width="+w+"px, height="+h+"px, toolbar=1, location=1, directories=1, status=1, menubar=1, scrollbars=1, resizable=1"); }
+    else
+    { newWindow=window.open(path,target,"left="+posx+", top="+posy+", width="+w+"px, height="+h+"px, toolbar=0, location=0, directories=0, status=0, menubar=0, scrollbars=1, resizable=1"); }
+    newWindow.focus();
+}
+//	новое окно 2
+function system_window2(path, w, h, pos, face)
+{
+    if ( pos==1 )
+    { posx=screen.width/2-w/2; posy=screen.height/2-h/2; }
+    else
+    { posx=0; posy=0; }
+    if ( face==1 )
+    { window.open(path,"","left="+posx+", top="+posy+", width="+w+"px, height="+h+"px, toolbar=1, location=1, directories=1, status=1, menubar=1, scrollbars=1, resizable=1"); }
+    else
+    { window.open(path,"","left="+posx+", top="+posy+", width="+w+"px, height="+h+"px, toolbar=0, location=0, directories=0, status=0, menubar=0, scrollbars=1, resizable=1"); }
+}
