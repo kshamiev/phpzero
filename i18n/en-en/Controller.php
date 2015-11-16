7a142ee4a83250ed0c05a59efdba6de7194e7086
Konstantin <kshamiev@hostkey.ru>
2015-11-16 13:28:33 +0300

    en-18 phpzero add

---
+++ i18n/en-en/Controller.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Константин
+ * Date: 12.05.14
+ * Time: 0:34
+ */
+return [
+    '' => '',
+    // Действия
+    'Action_Default' => 'Контроллер по умолчанию',
+    'Action_Remove' => 'удалить',
+    'Action_FilterSet' => 'применить фильтр',
+    'Action_FilterReset' => 'сбросить фильтр',
+    'Action_Add' => 'добавить',
+    'Action_Save' => 'сохранить',
+    'Action_Copy' => '',
+    'Action_Edit' => 'изменить',
+    'Action_CatalogMove' => 'переместить',
+    'Action_UpdateUrl' => 'обновить роутинг',
+    'Action_FolderGo' => 'переход по папкам',
+    'Action_FolderRemove' => 'удаление папки',
+    'Action_FileRemove' => 'удаление файла',
+    'Action_FileDownLoad' => 'отдача файла пользователю',
+    'Action_FileUpload' => 'загрузка файла на сервер',
+    'Action_FolderAdd' => 'создание папки',
+    'Action_EditFile' => 'изменение файла',
+    'Action_FileSave' => 'сохранение файла',
+    'Action_EngineModulesDB' => 'Инженеринг моделей',
+    'Action_CacheReset' => 'полный сброс кеша',
+    'Action_SessionReset' => 'сброс всех сессий',
+    'Action_Login' => 'Авторизация',
+    'Action_Reminder' => 'Восстановление пароля',
+    'Action_Logout' => 'Выход',
+    'Action_Profile' => 'Профиль',
+    // Результат действия
+    'Save' => 'сохранено',
+    'Reminder' => 'Письмо с реквизитами отправлено',
+    'Engine_Modules_DB' => 'Произведено',
+    // Ошибки
+    'Error_Registration' => 'Не зарегистрирован',
+    'Error_Password' => 'Пароль не верен',
+    'Error_Validate' => 'Ошибка валидации',
+    'Error_NotParam' => 'Параметры операции не определены',
+    'Error_NotFound' => 'Объект не найден',
+
+];
