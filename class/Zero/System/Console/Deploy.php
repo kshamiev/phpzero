de8062f9d4db6e8af67d1381fbb762edd43db717
Ilosa <kshamiev@hostkey.ru>
2018-05-18 14:03:23 +0300

    feature auto deploy Controller

---
+++ class/Zero/System/Console/Deploy.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Деплой или обновление проекта
+ *
+ * @package Zero.System.Console
+ * @author Konstantin Shamiev aka ilosa <konstantin@shamiev.ru>
+ * @date 2018.05.18
+ */
+class Zero_System_Console_Deploy extends Zero_Controller
+{
+    /**
+     * Формирование документации проекта
+     *
+     * @return int
+     */
+    public function Action_Default()
+    {
+        $path = dirname(ZERO_PATH_SITE);
+
+        // Выкладываем проект
+        foreach (Zero_App::$Config->Deploy->PathDeploy as $p)
+        {
+            if ( '/' == $p || '.' == $p || '/.' == $p || './' == $p )
+                $p = '';
+            $p = $path . $p;
+
+            $code = 0;
+            $buffer = [];
+            exec("cd {$p} && git checkout -f");
+            exec("cd {$p} && git clean -f -d");
+            exec("cd {$p} && git pull", $buffer, $code);
+            if ( 0 < $code )
+            {
+                Zero_Logs::Set_Message_Error("error git pull '{$p}'");
+                return 1;
+            }
+            else
+            {
+                Zero_Logs::Set_Message_Notice("git pull '{$p}'");
+            }
+        }
+        return 0;
+    }
+}
